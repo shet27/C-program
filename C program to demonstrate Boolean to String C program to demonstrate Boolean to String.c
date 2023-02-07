05ec38d99f2da466b14a631622183ce519ec59ca
Likith Shet <96128274+shet27@users.noreply.github.com>
2023-02-07 22:14:29 +0530

    Create C program to demonstrate Boolean to String C program to demonstrate Boolean to String.c

---
+++ C program to demonstrate Boolean to String C program to demonstrate Boolean to String.c
@@ -0,0 +1,16 @@
+// C program to demonstrate Boolean to String
+// Conversion using conditional statements
+#include <stdbool.h>
+#include <stdio.h>
+int main()
+{
+
+	bool n = true;
+	if (n == true) {
+		printf("true");
+	}
+	else {
+		printf("false");
+	}
+	return 0;
+}
