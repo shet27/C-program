8f4d1be20287bae008e20d814251046d5a5ef8c0
Likith Shet <96128274+shet27@users.noreply.github.com>
2023-05-13 20:28:07 +0530

    Update C program to demonstrate Boolean to String .c

--- C program to demonstrate Boolean to String .c
+++ C program to demonstrate Boolean to String .c
@@ -1,10 +1,8 @@
- 
-// C Program to demonstrate Boolean to Sting//
+ // C Program to demonstrate Boolean to Sting//
 #include <stdbool.h>
 #include <stdio.h>
 int main()
 {
-
 	bool n = true;
 	if (n == true) {
 		printf("true");
